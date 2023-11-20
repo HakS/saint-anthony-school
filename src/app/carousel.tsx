3aa9ff48fbaef98671648d4a3b0179c8b561401c
Juan Barba <Juan.Barba.O@gmail.com>
2023-11-20 10:07:05 -0500

    New layout proposal for sliders, show form instead of CTA

--- src/app/carousel.tsx
+++ src/app/carousel.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/autoplay'
 import { Autoplay, EffectCreative } from 'swiper/modules';
 import Image from 'next/image';
 import SimpleCTA from './simple-cta';
+import Button from './button';
 
 function SliderInfo(props: {text: string}) {
   return (
@@ -20,15 +21,17 @@ function SliderInfo(props: {text: string}) {
 }
 
 export default function Carousel() {
+  const formItemStyles = "mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50";
+
   return (
     <div className="w-full">
       <Swiper
         grabCursor={true}
         effect={'creative'}
         loop={true}
-        autoplay={{
-          delay: 5000,
-        }}
+        // autoplay={{
+        //   delay: 5000,
+        // }}
         creativeEffect={{
           prev: {
             shadow: true,
@@ -52,11 +55,51 @@ export default function Carousel() {
           <div className='absolute top-0 left-0 w-full h-full flex justify-center z-10'>
             <div className="max-w-screen-xl h-full w-full">
               <div className="h-full w-full md:w-1/2 flex flex-col justify-center p-10 drop-shadow-md text-white bg-purple-800 bg-opacity-40">
-                <SimpleCTA
-                  header={"Bienvenidos"}
-                  body={"Encabezado atractivo que resuma la esencia de la escuela. Breve mensaje de bienvenida destacando la amabilidad, la creatividad y la confianza en el ambiente educativo. Creatividad y Enfoque en Inglés"}
-                  cta={"Lea más"}
-                />
+                <div className="text-4xl font-bold w-full mb-5 text-white">¡Matricúlate yá!</div>
+                <div className="grid grid-cols-1 gap-6 text-white">
+                  <label className="block">
+                    <span>Nombres</span>
+                    <input
+                      type="text"
+                      className={`${formItemStyles}`}
+                      placeholder=""
+                    />
+                  </label>
+                  <label className="block">
+                    <span>Apellidos</span>
+                    <input
+                      type="text"
+                      className={`${formItemStyles}`}
+                      placeholder=""
+                    />
+                  </label>
+                  <label className="block">
+                    <span>Año escolar</span>
+                    <input
+                      type="number"
+                      className={`${formItemStyles}`}
+                      placeholder=""
+                    />
+                  </label>
+                  <label className="block">
+                    <span>Correo electrónico</span>
+                    <input
+                      type="email"
+                      className={`${formItemStyles}`}
+                      placeholder="juan.perez@ejemplo.com"
+                    />
+                  </label>
+                  <label className="block mb-4">
+                    <span>Teléfono</span>
+                    <input
+                      type="tel"
+                      multiple
+                      className={`${formItemStyles}`}
+                      placeholder="1234567 / 61234567"
+                    />
+                  </label>
+                  <Button text="Matricularme" />
+                </div>
               </div>
             </div>
           </div>
